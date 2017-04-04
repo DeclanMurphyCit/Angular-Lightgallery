86307a2095c213f38d34af4865837ff82209e1d6
Declan Murphy <declan@your.rentals>
2017-04-04 16:11:32 +0700

    Add files

---
+++ angular-lightgallery.js
@@ -0,0 +1,31 @@
+(function () {
+  angular
+    .module('shared.widgets')
+    .directive('photoGallery', photoGallery);
+
+  function photoGallery() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: '/photo-gallery.html',
+      link: link,
+      scope: {
+        images: '='
+      }
+    };
+    return directive;
+
+    function link(scope, element) {
+      scope.openGallery = openGallery;
+      function openGallery() {
+        element.parent().lightGallery({
+          thumbnail: true,
+          toogleThumb: false,
+          thumbWidth: 120,
+          thumbContHeight: 100,
+          dynamic: true,
+          dynamicEl: scope.images
+        });
+      }
+    }
+  }
+})();
